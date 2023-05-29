cd2d572edb3a11d8cceae5bf663f0ec8bfc6b9b2
Ravi@036# <admin@admins-MacBook-Pro.local>
2023-05-30 01:52:29 +0530

    Added: Lambda Expression

---
+++ src/test/java/com/bridgelabz/UserRegistrationTestUsingLambdaExpression.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistrationTestUsingLambdaExpression {
+
+    @Test
+    public void givenNameShouldReturnCorrectOrNot(){
+        System.out.println("Welcome to Lambda Expression assignment.");
+    }
+
+}
